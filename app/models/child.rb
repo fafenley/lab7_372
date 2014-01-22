c840c15c19856b59459a45acbe6d8bd0d5c1c1cc
Flavio Fenley <fafenley@gmail.com>
2014-01-21 20:29:03 -0500

    Child_Test commit

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,15 @@
 class Child < ActiveRecord::Base
   attr_accessible :active, :first_name, :last_name
+  has_many :chores
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  
+  scope :alphabetical, order(:first_name, :last_name)
+  scope :active, where(:active => true)
+
+  def name
+  	return "#{first_name} #{last_name}"
+  end
   
 end
