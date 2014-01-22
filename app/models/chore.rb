61f0cb74bd8be5519de98decf5f52c5547b92351
Flavio Fenley <fafenley@gmail.com>
2014-01-21 21:44:48 -0500

    Final Commit

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,24 @@
 class Chore < ActiveRecord::Base
   attr_accessible :child_id, :completed, :due_on, :task_id
-  
+  belongs_to :child
+  belongs_to :task 
+
+  validates_date :due_on
+
+  scope :by_task, joins(:task).order(:name)
+  scope :chronological, order(:due_on)
+
+  scope :past, where('due_on < ?', Date.today)
+  scope :upcoming, where('due_on >=?', Date.today)
+  scope :done, where(:completed => true)
+  scope :pending, where(:completed => false)
+
+
+  def status
+  	if self.completed
+  		"Completed"
+  	else
+  		"Pending"
+  	end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ActiveRecord::Base
   attr_accessible :active, :name, :points
-  
+  has_many(:chores)
+  validates_presence_of :name
+  validates :points, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+
+  scope :alphabetical, order(:name)
+  scope :active, where(:active => true)
+ 
 end
